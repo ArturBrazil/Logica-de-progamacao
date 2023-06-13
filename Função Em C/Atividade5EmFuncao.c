6dd467eace5378403ae1fdf8d92974273b97d3b7
ArturBrazil <128700537+ArturBrazil@users.noreply.github.com>
2023-06-13 08:28:48 -0300

    Update Atividade5EmFuncao.c

--- Função Em C/Atividade5EmFuncao.c
+++ Função Em C/Atividade5EmFuncao.c
@@ -2,36 +2,40 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void cabecalho(){
-printf("=============================\n");
-printf("  Calculo inflação de valor\n");
-printf("=============================\n");
-printf("\n");
+void cabecalho()
+{
+    printf("=============================\n");
+    printf("  Calculo inflaÃ§Ã£o de valor\n");
+    printf("=============================\n");
+    printf("\n");
 }
 
-void inflacao(float valor){
+void inflacao(float valor)
+{
     float valorInfla;
 
-   if (valor < 100)
-   {
-    valorInfla = valor+(valor* 0.1);
-   printf("Valor inflacionado em 10%%: %.2f", valorInfla);
-   }else
-   {
-    valorInfla = valor+(valor* 0.2);
-    printf("Valor inflacionado em 20%%: %.2f", valorInfla);
-   }    
+    if (valor < 100)
+    {
+        valorInfla = valor + (valor * 0.1);
+        printf("Valor inflacionado em 10%%: %.2f", valorInfla);
+    }
+    else
+    {
+        valorInfla = valor + (valor * 0.2);
+        printf("Valor inflacionado em 20%%: %.2f", valorInfla);
+    }
 }
-int main(){
-setlocale(LC_ALL, "");
+int main()
+{
+    setlocale(LC_ALL, "");
     float valor;
-cabecalho();
-printf("Informe o valor desejado: \n");
-scanf("%f", &valor);
+    cabecalho();
+    printf("Informe o valor desejado: \n");
+    scanf("%f", &valor);
 
-system("cls");
+    system("cls");
 
-inflacao(valor);
+    inflacao(valor);
 
-return 0;
+    return 0;
 }
