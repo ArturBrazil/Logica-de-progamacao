c283fdbf7ff6709ed27b0d40af57e93f7f449128
ArturBrazil <128700537+ArturBrazil@users.noreply.github.com>
2023-06-13 10:14:13 -0300

    Update Atividade6EmFuncao.c

--- Função Em C/Atividade6EmFuncao.c
+++ Função Em C/Atividade6EmFuncao.c
@@ -1,39 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-float media(float nota[]){
-float valorMedia = 0, soma = 0;
-int i;
-for (int i = 0; i < 3; i++) {
-soma+= nota[i];
-}
-
-valorMedia = soma / 3
-
-printf("Sua media é %.2f", valorMedia);
-if (valorMedia > 7)
+float media(float nota[])
 {
-    printf("Aprovado!");
-}else
+    float valorMedia = 0, soma = 0;
+    int i;
+    for (int i = 0; i < 3; i++)
+    {
+        soma += nota[i];
+    }
+
+    valorMedia = soma / 3
+
+                 printf("Sua media é %.2f", valorMedia);
+    if (valorMedia > 7)
+    {
+        printf("Aprovado!");
+    }
+    else
+    {
+        printf("Reprovado!");
+    }
+}
+
+int main()
 {
-    printf("Reprovado!");
-}
-}
+    int i;
+    float nota[3];
 
-int main(){
-int i;
-float nota[3];
+    for (i = 0; i < 3; i++)
+    {
+        printf("Informe sua nota: \n");
+        scanf("%f", &nota[i]);
+    }
 
-  
+    media(nota);
 
-for (i = 0; i < 3; i++)
-{
-    printf("Informe sua nota: \n");
-    scanf("%f", &nota[i]);
-}
-
-media(nota);
-
-return 0;
+    return 0;
 }
