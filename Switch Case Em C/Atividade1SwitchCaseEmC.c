8841256e277ced84ca9ae20445cd81919293ef40
ArturBrazil <128700537+ArturBrazil@users.noreply.github.com>
2023-06-16 10:40:27 -0300

    Update Atividade1SwitchCaseEmC.c

--- Switch Case Em C/Atividade1SwitchCaseEmC.c
+++ Switch Case Em C/Atividade1SwitchCaseEmC.c
@@ -6,7 +6,7 @@
 void cabecalho(){
 printf("[1] - Domingo \n");
 printf("[2] - Segunda \n");
-printf("[3] - Terça \n");
+printf("[3] - TerÃ§a \n");
 printf("[4] - Quarta \n");
 printf("[5] - Quinta \n");
 printf("[6] - Sexta \n");
@@ -18,7 +18,6 @@ int main(){
 int dia;
 setlocale(LC_ALL, ""); //PT-BR
 
-
 do{
 cabecalho();
 
@@ -29,30 +28,21 @@ switch (dia){
 case 1:
     printf("Final de Semana \n");
     break;
-case 2:
-    printf("Dia Útil. \n");
-    break;
-case 3:
-    printf("Dia Útil. \n");
-    break;
-case 4:
-    printf("Dia Útil. \n");
-    break;
-case 5:
-    printf("Dia Útil. \n");
-    break;
-case 6:
-    printf("Dia Útil. \n");
-    break;
 case 7:
     printf("Final de Semana \n");
+    break;
+case 2:
+case 3:
+case 4:
+case 5:
+case 6:
+    printf("Dia Ãštil. \n");
     break;
 case 0:
     printf("Encerrando o programa... \n");
     break;
-
 default:
-    printf("Opção Inválida, Tente novamente! \n");
+    printf("OpÃ§Ã£o InvÃ¡lida, Tente novamente! \n");
     break;
 
 }
