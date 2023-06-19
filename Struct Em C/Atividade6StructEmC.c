cec10a071622da6920c6a8c56381660be257c0f6
ArturBrazil <128700537+ArturBrazil@users.noreply.github.com>
2023-06-19 16:55:48 -0300

    Update Atividade6StructEmC.c

--- Struct Em C/Atividade6StructEmC.c
+++ Struct Em C/Atividade6StructEmC.c
@@ -3,43 +3,39 @@
 #include <locale.h>
 
 struct classe {
-    char aluno[2][250];
-    int codDis[2];
-    float nota1, nota2;
+	char aluno[2][250];
+	int id[2];
+	float nota1, nota2;
 };
 
 int main() {
-    setlocale(LC_ALL, "");
-    struct classe sala[2];
-    int i;
-    float mediaGeral = 0;
-    float soma = 0;
+	setlocale(LC_ALL,""); //PT-BR
+	struct classe lista[2];
+	float media, maior, menor, par;
+	int i;
 
-    for (i = 0; i < 2; i++) {
-        printf("Informe seu nome: \n");
-        fflush(stdin);
-        gets(sala[i].aluno[i]);
-        fflush(stdin);
-        printf("Informe o código da disciplina: \n");
-        scanf("%d", &sala[i].codDis[i]);
-        printf("Informe sua primeira nota: \n");
-        scanf("%f", &sala[i].nota1);
-        printf("Informe sua segunda nota: \n");
-        scanf("%f", &sala[i].nota2);
-        soma += sala[i].nota1 + sala[i].nota2;
-        mediaGeral += (sala[i].nota1 + sala[i].nota2) / 2;
-    }
+	for (i=0; i<2; i++) {
+		printf("Informe o cÃ³digo da disciplina: \n");
+		scanf("%d", &lista[i].id[i]);
+		printf("Informe seu nome: \n");
+		fflush(stdin);
+		gets(lista[i].aluno[i]);
+		fflush(stdin);
+		printf("Informe a primeira nota: \n");
+		scanf("%f", &lista[i].nota1);
+		printf("Informe a segunda nota: \n");
+		scanf("%f", &lista[i].nota2);
+		media += (lista[i].nota1+lista[i].nota1) / 2;
 
-    system("cls");
+	}
 
-    for (i = 0; i < 2; i++) {
-        printf("Nome: %s\n", sala[i].aluno[i]);
-        printf("Cód da Disciplina: %d\n", sala[i].codDis[i]);
-        printf("Nota1: %.2f\n", sala[i].nota1);
-        printf("Nota2: %.2f\n", sala[i].nota2);
-        printf("Média: %.2f\n", (sala[i].nota1 + sala[i].nota2) / 2);
-        printf("\n");
-    }
+	system("cls");
 
-    return 0;
+	for(i=0; i<2; i++) {
+		printf("Nome: %s \n", lista[i].aluno[i]);
+        printf("Primeira nota: %.2f \n", lista[i].nota1);
+		printf("Segunda nota: %.2f \n", lista[i].nota2);
+        printf("Disciplina: %d \n", lista[i].id[i]);
+		printf("Media: %.2f \n", (lista[i].nota1+lista[i].nota2)/2);
+	}
 }
